342072171300487eb332a97387102c6828c9860d
Francisco Morales <franmorales@gmail.com>
2017-02-04 10:10:04 +0100

    phonegap-maps

---
+++ README.md
@@ -0,0 +1,4 @@
+# Maps
+
+Una aplicación simple de PhoneGap que usa geolocalización y mapas de Leaflet
+
---
+++ www/js/maps.js
@@ -0,0 +1,54 @@
+var app = {
+  inicio: function() {
+    this.iniciaFastClick();
+
+  },
+
+  iniciaFastClick: function () {
+    FastClick.attach(document.body);
+  },
+
+  dispositivoListo: function(){
+    navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
+  },
+
+  pintaCoordenadasEnMapa: function(position){
+    var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibW9yYWxlc2dlYSIsImEiOiJjaXlocGswNWkwNWNmMzhyd2ppaWM4OXVxIn0.vhg2eIWwqA5zO4iKG2BtTw', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(miMapa);
+
+    //L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //  maxZoom: 19,
+    //  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    //}).addTo(miMapa);
+
+    app.pintaMarcador([position.coords.latitude, position.coords.longitude], '¡Estoy aquí!', miMapa);
+
+    miMapa.on('click', function(evento){
+      var texto = 'Marcador en l(' + evento.latlng.lat.toFixed(2) + ') y L(' + evento.latlng.lng.toFixed(2) + ')';
+      app.pintaMarcador(evento.latlng, texto, miMapa);
+    });
+  },
+
+  pintaMarcador: function(latlng, texto, mapa){
+    var marcador = L.marker(latlng).addTo(mapa);
+    marcador.bindPopup(texto).openPopup();
+  },
+
+  errorAlSolicitarLocalizacion: function(error){
+    console.log(error.code + ': ' + error.message);
+  }
+
+};
+
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    app.inicio();
+  }, false);
+  document.addEventListener('deviceready', function() {
+    app.dispositivoListo();
+  }, false);
+}
